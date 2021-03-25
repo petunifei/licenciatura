0ea23960b3c19944ff2da5cb6faaaddf680f76bb
petunifei <70551493+petunifei@users.noreply.github.com>
2021-03-25 20:41:14 -0300

    Add files via upload

---
+++ js/pet.js
@@ -0,0 +1,68 @@
+
+/*ensino*/
+$(function(){
+		$('.but-curso-fis').click(function(){
+			$(this).siblings('.disciplina-fis').slideToggle();
+		}
+		);
+	});
+		$(function(){
+				$('.but-fisica-geral-1').click(function(){
+					$(this).siblings('.materia-fisica-geral-1').slideToggle();
+			}
+			);
+		});
+				$(function(){
+					$('.but-materia-cinematica').click(function(){
+						$(this).siblings('.content-cinematica').slideToggle().css('display', 'flex');
+					}
+					);
+				});
+				$(function(){
+					$('.but-materia-dinamica').click(function(){
+						$(this).siblings('.content-dinamica').slideToggle().css('display', 'flex');
+					}
+					);
+				});
+
+
+
+
+$(function(){
+	$('.but-curso-mat').click(function(){
+		$(this).siblings('.disciplina-mat').slideToggle();
+	}
+	);
+});
+		$(function(){
+			$('.but-mat-calculo-1').click(function(){
+			$(this).siblings('.materia-calculo-1').slideToggle();
+			}
+			);
+		});
+				$(function(){
+					$('.but-materia-cinematica').click(function(){
+					$(this).siblings('.content-fucaoes').slideToggle().css('display', 'flex');
+					}
+					);
+				});
+		$(function(){
+			$('.but-mat-ga').click(function(){
+			$(this).siblings('.materia-ga').slideToggle();
+			}
+			);
+		});
+				$(function(){
+					$('.but-materia-matrizes').click(function(){
+					$(this).siblings('.content-matrizes').slideToggle().css('display', 'flex');
+					}
+					);
+				});
+
+
+
+
+
+
+
+
